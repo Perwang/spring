12b30885b28d9d25671eb91c74a134548a720620
Perwang <wcp88888888@163.com>
2015-03-25 16:08:33 +0800

    添加了aop的实现
    实现aop

---
+++ src/main/java/com/wang/spring/aop/test/LogUtils.java
@@ -0,0 +1,8 @@
+package com.wang.spring.aop.test;
+
+public class LogUtils {
+	
+	public void sayLog(){
+		System.out.println("我是日志");
+	}
+}
---
+++ src/main/java/com/wang/spring/aop/test/UserService.java
@@ -0,0 +1,12 @@
+package com.wang.spring.aop.test;
+
+/**
+ * 
+ * @author wangcanpei
+ *
+ */
+public class UserService {
+	public void sayName(){
+		System.out.println("我是王灿佩");
+	}
+}
